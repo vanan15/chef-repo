7bb057bd4dcb21c8d7ed5dad591b5986eee2155b
An Dinh ianstg <no@domail.com>
2016-06-09 03:01:47 +1000

    Update homepage

--- opsworks/recipes/deploy.rb
+++ opsworks/recipes/deploy.rb
@@ -2,3 +2,11 @@ event = 'Deploy'
 bash 'make_curl' do
   code "curl -i -X POST -d 'name=#{event}' http://putsreq.com/lxq1nPBZNH8s7HCQcR72"
 end
+#Install source code
+remote_file '/var/www/html/index.html' do
+  source 'https://raw.githubusercontent.com/vanan15/testrepo/master/index.html'
+  owner 'www_data'
+  group 'www_data'
+  mode '0644'
+  action :create
+end
