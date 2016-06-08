feed321b2f57d2d699411671fe43ded2eca098f7
An Dinh ianstg <no@domail.com>
2016-06-09 00:50:09 +1000

    Install apache

--- opsworks/recipes/setup.rb
+++ opsworks/recipes/setup.rb
@@ -2,3 +2,7 @@ event = 'Setup'
 bash 'make_curl' do
   code "curl -i -X POST -d 'name=#{event}' http://putsreq.com/lxq1nPBZNH8s7HCQcR72"
 end
+#Install apache2
+package "apache2" do
+  action :upgrade
+end
